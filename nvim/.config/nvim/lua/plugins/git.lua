d23d2eb198a9680e9d1d64fbdb4e4c564bd8b52e
Filippo831 <filippoarduini83@gmail.com>
2025-10-07 09:12:33 +0200

    feat: git worktree in nvim

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -3,12 +3,21 @@ local function copilot_config()
     vim.g.copilot_assume_mapped = true
     vim.g.copilot_enabled = false
     vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-end 
+end
+
+local git_worktree_keys = {
+    { "<leader>gwc", function() require("telescope").extensions.git_worktree.create_git_worktree() end },
+    { "<leader>gws", function() require("telescope").extensions.git_worktree.git_worktrees() end },
+}
 
 return {
     "tpope/vim-fugitive",
     {
         "github/copilot.vim",
         config = copilot_config
-    }
+    },
+    {
+        "ThePrimeagen/git-worktree.nvim",
+        keys = git_worktree_keys,
+    },
 }
