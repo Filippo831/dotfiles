746904cf8e865c7d86e4f04f613b0fd3d7a279ca
Filippo831 <filippoarduini83@gmail.com>
2025-10-23 15:12:33 +0200

    changes

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -2,7 +2,7 @@ local function copilot_config()
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
     vim.g.copilot_enabled = false
-    vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<C-s>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 end
 
 local git_worktree_keys = {
